903872b170d894be532efc7180f216d6ae7dde63
Harshal Bhalerao <bhaleraoharshal99@gmail.com>
2023-12-18 12:56:51 +0400

    Added Leetcode 167 Solution

---
+++ 167_two_sum_2/sol.py
@@ -0,0 +1,17 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l, r = 0, len(numbers) - 1
+        result = []
+
+        while l < r:
+            sum = numbers[l] + numbers[r]
+            if sum == target:
+                return [l+1, r+1]
+            elif sum > target:
+                r -= 1
+            elif sum < target:
+                l += 1
+
+        return []
+
+                
